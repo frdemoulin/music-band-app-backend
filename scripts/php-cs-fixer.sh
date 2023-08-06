21529ca937fb961d543d94a7a164609c91600959
Frederic Demoulin <freddmn08@gmail.com>
2023-08-06 20:46:16 +0200

    fix: configuration de PHP-CS-FIXER

--- scripts/php-cs-fixer.sh
+++ scripts/php-cs-fixer.sh
@@ -2,4 +2,4 @@ PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 
 echo -e "${PURPLE}#### PHP CS FIXER ####${NC}"
-tools/php-cs-fixer/vendor/bin/php-cs-fixer fix -vvv
+tools/php-cs-fixer/vendor/bin/php-cs-fixer fix -vvv --dry-run
